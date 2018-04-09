68b26730fc6102fa3974c3e26b96471961a5ea50
asaldana <asaldana@housell.com>
2018-04-09 19:02:48 +0200

    accordion

--- _js/main.js
+++
@@ -1,7 +0,0 @@
-$('.u-hslform__combo-field').keyup( function() {
-  if (this.value === "" ) {
-    $(this).parent().addClass('u-hslform__combo--is-empty');
-  } else {
-    $(this).parent().removeClass('u-hslform__combo--is-empty');
-    }
-})
